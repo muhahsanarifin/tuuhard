8a4f785fa6a8b2efe42f026a2eeac93b1ad519cb
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-07-14 18:42:48 +0700

    Add icon.ts

---
+++ utils/others/icon.ts
@@ -0,0 +1,58 @@
+// Icons source : https://iconify.design/
+
+// hFlip = Horizontal flip
+
+export const sideBar = [
+  {
+    fitur: "Home",
+    icon: "mdi:home",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: false,
+  },
+  {
+    fitur: "Transaction",
+    icon: "bi:list-task",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: false,
+  },
+  {
+    fitur: "Casier",
+    icon: "ion:people-sharp",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: true,
+  },
+  {
+    fitur: "Settings",
+    icon: "fluent:settings-28-filled",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: false,
+  },
+];
+
+export const sorter = {
+  fitur: "More",
+  icon: "icon-park-outline:more",
+  color: "#f4ffff",
+  width: 24,
+  height: 24,
+};
