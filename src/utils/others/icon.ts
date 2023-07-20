271046ee70092e0c4bf035298d06b085c76e451b
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-07-20 10:50:01 +0700

    Add icon.ts

---
+++ src/utils/others/icon.ts
@@ -0,0 +1,82 @@
+// Icons source : https://iconify.design/
+
+// hFlip = Horizontal flip
+
+export const sideBar = [
+  {
+    fitur: "Home",
+    icon: "mdi:home",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: false,
+  },
+  {
+    fitur: "Transaction",
+    icon: "bi:list-task",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: false,
+  },
+  {
+    fitur: "Casier",
+    icon: "ion:people-sharp",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: true,
+  },
+  {
+    fitur: "Settings",
+    icon: "fluent:settings-28-filled",
+    color: {
+      active: "#2882d7",
+      disable: "#afdaf9",
+    },
+    widh: 24,
+    height: 24,
+    horizontal_flip: false,
+  },
+];
+
+export const sorter = {
+  fitur: "More",
+  icon: "icon-park-outline:more",
+  color: "#f4ffff",
+  width: 24,
+  height: 24,
+};
+
+export const cart = {
+  close: {
+    fitur: "Close",
+    icon: "formkit:close",
+    color: "#b64d5f",
+    width: 24,
+    height: 24,
+  },
+  minus: {
+    fitur: "Minus",
+    icon: "ic:round-minus",
+    color: "#3f84c9",
+    width: 24,
+    height: 24,
+  },
+  plus: {
+    fitur: "Plus",
+    icon: "ic:round-plus",
+    color: "#3f84c9",
+    width: 24,
+    height: 24,
+  },
+};
