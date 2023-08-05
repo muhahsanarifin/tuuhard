80aaae6ed7d38d81b9e1fb1c61e3879452cb609e
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-08-06 04:56:06 +0700

    Update buttonType.ts

--- src/utils/types/buttonType.ts
+++ src/utils/types/buttonType.ts
@@ -2,3 +2,12 @@ export type CartButtonProps = {
   title: string;
   cls?: string;
 };
+
+export type ProductButtonProps = {
+  onClass: string;
+  icon: string;
+  color: string;
+  width: number;
+  height: number;
+  setClick?: () => void;
+};
