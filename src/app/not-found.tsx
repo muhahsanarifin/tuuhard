bdfec12db4995f134d4ea877406a0aff78a0c691
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-08-06 04:54:07 +0700

    Add not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as Tempalate from "../components/Template";
+
+const NotFound = () => {
+  return (
+    <>
+      <main>
+        <Tempalate.NotFound/>
+      </main>
+    </>
+  );
+};
+
+export default NotFound;
