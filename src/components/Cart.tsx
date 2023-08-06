bc5590f49cdadc61888df719a9b91a9ce25b0094
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-08-06 15:46:38 +0700

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,6 +6,7 @@ import { Icon } from "@iconify/react";
 import * as icon from "../utils/others/icon";
 import Image from "next/image";
 import * as Button from "../components/Button";
+import * as Motion from "./Motion";
 
 const Cart: React.FC<{}> = () => {
   const { close, minus, plus } = icon.cart;
@@ -24,41 +25,43 @@ const Cart: React.FC<{}> = () => {
             />
           </button>
         </div>
-        <ul className="p-4 flex-1 flex flex-col gap-y-2">
-          {new Array(3).fill(0).map((_, idx) => (
-            <li
-              key={idx}
-              className="border-b-2 bg-pic-bg border-solid border-primary-border-c flex p-2 rounded-lg"
-            >
-              <div className="flex">
-                <Image
-                  src="https://res.cloudinary.com/dgdhrpj1r/image/upload/v1684630439/tuuhard-products/pt-semangka.jpg"
-                  width={500}
-                  height={500}
-                  alt=""
-                  priority={true}
-                  className="w-[32px] h-[32px] m-auto rounded-md"
-                />
-              </div>
-              <div className="flex-1 mx-2">
-                <p className="text-sm text-rf-c">Fruit</p>
-                <p className="font-semibold text-bf-c">$100</p>
-              </div>
-              <div className="flex items-center basis-1/4">
-                <ul className="flex-1 flex justify-center items-center gap-x-1">
-                  <li className="bg-icon-bg w-[24px] h-[24px] rounded-full text-center">
-                    -
-                  </li>
-                  <li className="font-semibold text-sm text-bf-c">10</li>
-                  <li className="bg-icon-bg w-[24px] h-[24px] rounded-full text-center">
-                    +
-                  </li>
-                </ul>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="p-4 flex flex-col gap-y-2">
+        <Motion.fadeInUp>
+          <ul className="p-4 flex-1 flex flex-col gap-y-2">
+            {new Array(3).fill(0).map((_, idx) => (
+              <li
+                key={idx}
+                className="border-b bg-pic-bg border-solid border-primary-border-c flex p-2 rounded-lg"
+              >
+                <div className="flex">
+                  <Image
+                    src="https://res.cloudinary.com/dgdhrpj1r/image/upload/v1684630439/tuuhard-products/pt-semangka.jpg"
+                    width={500}
+                    height={500}
+                    alt=""
+                    priority={true}
+                    className="w-[32px] h-[32px] m-auto rounded-md"
+                  />
+                </div>
+                <div className="flex-1 mx-2">
+                  <p className="text-sm text-rf-c">Fruit</p>
+                  <p className="font-semibold text-bf-c">$100</p>
+                </div>
+                <div className="flex items-center basis-1/4">
+                  <ul className="flex-1 flex justify-center items-center gap-x-1">
+                    <li className="bg-icon-bg w-[24px] h-[24px] rounded-full text-center">
+                      -
+                    </li>
+                    <li className="font-semibold text-sm text-bf-c">10</li>
+                    <li className="bg-icon-bg w-[24px] h-[24px] rounded-full text-center">
+                      +
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Motion.fadeInUp>
+        <div className="mt-auto p-4 flex flex-col gap-y-2">
           <div className="flex">
             <p className="font-semibold text-bf-c">Subtotal</p>
             <p className="ml-auto font-semibold text-bf-c">$0</p>
